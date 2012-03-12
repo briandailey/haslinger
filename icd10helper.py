27f8dccb989cbb14fbe3e7aa1a95a48ce87bd6b7
Brian Dailey <brian@stratasan.com>
2012-03-12 15:14:55 -0700

    Adding templates, bootstrap, jquery UI.

--- icd10helper.py
+++ icd10helper.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flaskext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,8 +6,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 class Icd10Code(db.Model):
     id = db.Column(db.Integer, primary_key=True)
