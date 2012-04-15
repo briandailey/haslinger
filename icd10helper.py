cee57ae27715b67694949ff06e7a3ad9e2f54a8a
Brian Dailey <brian@stratasan.com>
2012-04-14 21:21:20 -0500

    Moving jquery-ui.

--- icd10helper.py
+++ icd10helper.py
@@ -1,32 +1,156 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sqlalchemy import or_, and_
 from flaskext.sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/brian/Projects/icd10helper.com/db/test.db'
 db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/search", methods=['GET'])
+def search():
+    forward = request.args.get('forward')
+    diagnosis = request.args.get('diagnosis')
+    q = request.args.get('q')
+    if forward == '1':
+        matches = Icd9Code.query.filter(
+                or_(
+                    Icd9Code.code.like("%%%s%%" % q),
+                    Icd9Code.description.like("%%%s%%" % q),
+                ),
+
+        ).filter(Icd9Code.diagnosis==diagnosis).limit(10)
+        # todo: add implied decimal to description
+    else:
+        matches = Icd10Code.query.filter(
+                or_(
+                    Icd10Code.code.like("%%%s%%" % q.replace('.', '')),
+                    Icd10Code.description.like("%%%s%%" % q),
+                ),
+        ).all()
+
+    if diagnosis:
+        return json.dumps([dict( label="%s.%s - %s" % (m.code[0:3], m.code[3:], m.description), value="%s.%s" % (m.code[0:3], m.code[3:]),) for m in matches])
+    else:
+        return json.dumps([dict( label="%s.%s - %s" % (m.code[0:2], m.code[2:], m.description), value="%s.%s" % (m.code[0:2], m.code[2:]),) for m in matches])
+
+@app.route("/gem", methods=['GET'])
+def gem():
+    forward = request.args.get('forward') == "1"
+    diagnosis = request.args.get('diagnosis')
+    q = request.args.get('q')
+    code = q.replace('.', '').strip()
+    # If going forward, starting with an icd9 code.
+
+    valid_code = False
+    matches = []
+    if forward:
+        if Icd9Code.query.filter(and_(Icd9Code.code==code, Icd9Code.diagnosis==diagnosis)).count() == 1:
+            valid_code = True
+            matches = Mapper.query.filter(
+                    and_(
+                        Mapper.forward==True,
+                        Mapper.icd9code==code,
+                        Mapper.diagnosis==diagnosis,)
+                ).all()
+    else:
+        if Icd10Code.query.filter(and_(Icd10Code.code==code, Icd10Code.diagnosis==diagnosis)).count() == 1:
+            valid_code = True
+            matches = Mapper.query.filter(
+                    and_(
+                        Mapper.forward==False,
+                        Mapper.icd10code==code,
+                        Mapper.diagnosis==diagnosis,)
+                ).all()
+    return render_template('gem.html', 
+            q=q,
+            matches=matches,
+            forward=forward,
+            diagnosis=diagnosis,
+            valid_code=valid_code,
+            )
+
+## MODELS ##
 class Icd10Code(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    order_no = db.Column(db.String(5), unique=True) # Order number
-    icd10code = db.Column(db.String(5), unique=True) # ICD-10-CM or ICD-10-PCS code. Dots are not included
+    order_no = db.Column(db.String(5)) # Order number
+    code = db.Column(db.String(5)) # ICD-10-CM or ICD-10-PCS code. Dots are not included
     ub04_valid = db.Column(db.Boolean, default=False) # Valid for submission on a UB04
-    short_desc = db.Column(db.String(60)) # Short description
+    description = db.Column(db.String(60)) # Short description
     long_desc = db.Column(db.String(300)) # Long description
+    diagnosis = db.Column(db.Boolean, default=False) # If not a diagnosis, then a procedure.
 
-class Icd10Map(db.Model):
+class Icd9Code(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(16))
+    description = db.Column(db.String(255))
+    diagnosis = db.Column(db.Boolean, default=False)
+
+    def code_formatted(self):
+        if self.diagnosis:
+            breakpoint = 2
+        else:
+            breakpoint = 3
+        return "%s.%s" % (self.icd9code[0:breakpoint], self.icd9code[breakpoint:])
+
+
+class Mapper(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     forward = db.Column(db.Boolean, default=False)
     icd10code = db.Column(db.String(5))
     icd9code = db.Column(db.String(5))
+    diagnosis = db.Column(db.Boolean, default=False) # If not a diagnosis, then a procedure.
     approximate = db.Column(db.Boolean, default=False)
     no_map = db.Column(db.Boolean, default=False)
     combination = db.Column(db.Boolean, default=False)
     scenario = db.Column(db.Integer)
     choice_list = db.Column(db.Integer)
 
+    def icd9code_formatted(self):
+        if self.icd9code == 'NoDx':
+            return self.icd9code
+        print self.diagnosis
+        if self.diagnosis:
+            breakpoint = 2
+        else:
+            breakpoint = 3
+        return "%s.%s" % (self.icd9code[0:breakpoint], self.icd9code[breakpoint:])
+
+    def icd10code_formatted(self):
+        if self.icd10code == 'NoDx':
+            return self.icd9code
+        if self.diagnosis:
+            breakpoint = 3
+        else:
+            breakpoint = 4
+        return "%s.%s" % (self.icd10code[0:breakpoint], self.icd10code[breakpoint:])
+
+    def icd10code_description(self):
+        """ Not terribly efficient, no. """
+        if self.icd10code == 'NoDx':
+            return ''
+        icd10code = Icd10Code.query.filter(Icd10Code.code==self.icd10code).first()
+        if icd10code:
+            return icd10code.long_desc
+        else:
+            return ''
+
+    def icd9code_description(self):
+        if self.icd9code == 'NoDx':
+            return ''
+        icd9code = Icd9Code.query.filter(Icd9Code.code==self.icd9code).first()
+        if icd9code:
+            return icd9code.description
+        else:
+            return ''
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- load.py
+++ load.py
@@ -1,22 +1,63 @@
-from icd10helper import Icd10Code, db, Icd10Map
+from icd10helper import Icd10Code, db, Mapper, Icd9Code
+import codecs
 
 def read_icd10_codes():
+    # Diagnosis
     with open('sources/icd10cm_order_2012.txt', 'r') as f:
         for line in f.readlines():
             code = Icd10Code(
                     order_no=line[0:5],
-                    icd10code=line[6:13].strip(),
+                    code=line[6:13].strip(),
                     ub04_valid=line[14:15],
-                    short_desc=line[17:75].strip(),
-                    long_desc=line[77:].strip()
+                    description=line[16:75].strip(),
+                    long_desc=line[77:].strip(),
+                    diagnosis=True,
             )
             db.session.add(code)
-        db.session.commit()
 
-def read_icd10_gem_file():
+    # pcs - procedures
+    with open('sources/icd10pcs_order_2012.txt', 'r') as f:
+        for line in f.readlines():
+            code = Icd10Code(
+                    order_no=line[0:5],
+                    code=line[6:13].strip(),
+                    ub04_valid=line[14:15],
+                    description=line[16:75].strip(),
+                    long_desc=line[77:].strip(),
+                    diagnosis=False,
+            )
+            db.session.add(code)
+
+    db.session.commit()
+
+def read_icd9_codes():
+    # dx
+    with codecs.open('sources/CMS29_DESC_LONG_DX.101111.txt', 'rb', encoding='latin-1') as f:
+        for line in f.readlines():
+            code = Icd9Code(
+                    diagnosis=True,
+                    code=line[0:6].strip(),
+                    description=line[6:].strip(),
+                )
+            db.session.add(code)
+    # px
+    with codecs.open('sources/CMS29_DESC_LONG_SG.txt', 'rb', encoding='latin-1') as f:
+        for line in f.readlines():
+            code = Icd9Code(
+                    diagnosis=False,
+                    code=line[0:4].strip(),
+                    description=line[5:].strip()
+                )
+            db.session.add(code)
+    db.session.commit()
+
+
+
+def read_icd10_gem_files():
+    # DX
     with open('sources/2012_I10gem.txt', 'r') as f:
         for line in f.readlines():
-            code = Icd10Map(
+            code = Mapper(
                     forward = False,
                     icd10code = line[0:7].strip(),
                     icd9code = line[8:13].strip(),
@@ -24,27 +65,61 @@ def read_icd10_gem_file():
                     no_map = line[15:16],
                     combination = line[16:17],
                     scenario = line[17:18],
-                    choice_list = line[18:19]
+                    choice_list = line[18:19],
+                    diagnosis = True,
             )
             db.session.add(code)
-        db.session.commit()
-
-def read_icd9_gem_file():
-    with open('sources/2012_I9gem.txt', 'r') as f:
+    # procedures
+    with open('sources/gem_pcsi9.txt', 'r') as f:
         for line in f.readlines():
-            code = Icd10Map(
-                    forward = True,
-                    icd9code = line[0:7].strip(),
-                    icd10code = line[8:13].strip(),
+            code = Mapper(
+                    forward = False,
+                    icd10code = line[0:7].strip(),
+                    icd9code = line[8:13].strip(),
                     approximate = line[14:15],
                     no_map = line[15:16],
                     combination = line[16:17],
                     scenario = line[17:18],
-                    choice_list = line[18:19]
+                    choice_list = line[18:19],
+                    diagnosis = False,
             )
             db.session.add(code)
-        db.session.commit()
+    db.session.commit()
+
+def read_icd9_gem_files():
+    with open('sources/2012_I9gem.txt', 'r') as f:
+        for line in f.readlines():
+            mapping = Mapper(
+                    forward = True,
+                    icd9code = line[0:5].strip(),
+                    icd10code = line[6:13].strip(),
+                    approximate = line[14:15],
+                    no_map = line[15:16],
+                    combination = line[16:17],
+                    scenario = line[17:18],
+                    choice_list = line[18:19],
+                    diagnosis = True,
+            )
+            db.session.add(mapping)
+    with open('sources/gem_pcsi9.txt', 'r') as f:
+        for line in f.readlines():
+            mapping = Mapper(
+                    forward = True,
+                    icd9code = line[0:5].strip(),
+                    icd10code = line[6:13].strip(),
+                    approximate = line[14:15],
+                    no_map = line[15:16],
+                    combination = line[16:17],
+                    scenario = line[17:18],
+                    choice_list = line[18:19],
+                    diagnosis = False,
+
+            )
+            db.session.add(mapping)
+    db.session.commit()
 
 if __name__ == "__main__":
-    read_icd10_gem_file()
-    read_icd9_gem_file()
+    read_icd10_gem_files()
+    read_icd9_gem_files()
+    read_icd10_codes()
+    read_icd9_codes()
